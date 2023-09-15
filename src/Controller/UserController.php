eea9095acc996f9f962f98218dfebc1cf5faa6bf
Titi <tristanfio05@gmail.com>
2023-09-15 10:51:29 +0200

    Sign Presence

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,18 +5,42 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
 class UserController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    public function index(UserRepository $userRepository): Response
-    {
-        $users = $userRepository->findOnlyUsers(); 
+    private ManagerRegistry $doctrine;
 
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function index(): Response
+    {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
-            'users' => $users,
         ]);
+    }
+
+    #[Route('/sign', name: 'app_signature')]
+    public function signe(): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('user/sign.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/update-sign', name: 'app_update_signature')]
+    public function updateSign() : Response
+    {
+        $user = $this->getUser();
+        $user->setIsSign(true);
+        $this->doctrine->getManager()->flush();
+        
+        return $this->redirectToRoute('app_profile');
     }
 }
